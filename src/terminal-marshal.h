0be73c64e379b234fa63cd4098f729f7cc9ed947
Michael Vogt <michael.vogt@ubuntu.com>
2008-06-18 10:33:45 +0200

    Import patches-unapplied version 2.23.4.2-0ubuntu1 to ubuntu/intrepid
    Imported using git-ubuntu import.
    
    Changelog parent: 5f5ee08f792e93b7d5fa44e7e6965f7d3da9e4be
    
    New changelog entries:
      * new upstream version
      * changed debian/patches/01_lpi.patch to work with GtkUIManager
      * dropped debian/patches/03_ubuntu_lpi_fix_crash.diff because the
        UIManager makes it obsolete
      * updated and renamed debian/patches/02_autoconf.patch to
        debian/patches/99_autoreconf.patch
      * debian/control.in:
        - add Vcs-Bzr header
      * debian/watch:
        - look for uneven version numbers too to make bzr-buildpackage
          auto download the right tarball
      * debian/gnome-terminal-data.install:
        - drop /usr/share/pixmaps, its no longer used

---
+++ src/terminal-marshal.h
@@ -0,0 +1,20 @@
+
+#ifndef ___terminal_marshal_MARSHAL_H__
+#define ___terminal_marshal_MARSHAL_H__
+
+#include	<glib-object.h>
+
+G_BEGIN_DECLS
+
+/* VOID:STRING,INT (../../../src/terminal-marshal.list:1) */
+G_GNUC_INTERNAL void _terminal_marshal_VOID__STRING_INT (GClosure     *closure,
+                                                         GValue       *return_value,
+                                                         guint         n_param_values,
+                                                         const GValue *param_values,
+                                                         gpointer      invocation_hint,
+                                                         gpointer      marshal_data);
+
+G_END_DECLS
+
+#endif /* ___terminal_marshal_MARSHAL_H__ */
+
