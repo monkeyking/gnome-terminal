64f0f6d6610035154b52fa1803913c1c4e31933d
Josselin Mouette <joss@debian.org>
2008-09-18 16:17:00 +0200

    Import patches-unapplied version 2.22.3-3 to debian/lenny
    Imported using git-ubuntu import.

---
+++ src/skey/skeyutil.c
@@ -0,0 +1,21 @@
+#include <config.h>
+
+#include <ctype.h>
+#include "skeyutil.h"
+
+void skey_sevenbit(char *s)
+{
+	char *p;
+
+	for (p = s; *p; p++)
+		*p &= 0x7f;
+}
+
+void skey_lowcase(char *s)
+{
+	char *p;
+
+	for (p = s; *p; p++)
+		if (isupper(*p))
+			*p = tolower(*p);
+}
