29195914a1327758ad0bc3b775928c9fecfc6b79
Robert Ancell <robert.ancell@canonical.com>
2015-10-27 16:38:22 +1300

    Import patches-unapplied version 3.18.1-0ubuntu1 to ubuntu/xenial-proposed
    Imported using git-ubuntu import.
    
    Changelog parent: 41f2b4818919479ec203e923c07064ed4893e22f
    
    New changelog entries:
      * New upstream release
      * debian/control:
        - Bump build-depends on libglib2.0-dev, libvte-2.91-dev, valac

--- src/terminal-notebook.h
+++ src/terminal-notebook.h
@@ -50,6 +50,13 @@ GType terminal_notebook_get_type (void);
 
 GtkWidget *terminal_notebook_new (void);
 
+void terminal_notebook_set_tab_policy (TerminalNotebook *notebook,
+                                       GtkPolicyType policy);
+GtkPolicyType terminal_notebook_get_tab_policy (TerminalNotebook *notebook);
+
+GtkWidget *terminal_notebook_get_action_box (TerminalNotebook *notebook,
+                                             GtkPackType pack_type);
+
 G_END_DECLS
 
 #endif /* TERMINAL_NOTEBOOK_H */
