0686cd602d9c0e48d772b2eb3c4da99c5e69ab3d
Iain Lane <iain@orangesquash.org.uk>
2016-03-15 12:03:08 +0000

    add a handler for launchpad bug URLs.
    Gbp-Pq: 60_add_lp_handler.patch.

--- src/terminal-screen.c
+++ src/terminal-screen.c
@@ -171,6 +171,7 @@ static const TerminalRegexPattern url_regex_patterns[] = {
   { REGEX_URL_VOIP,  FLAVOR_VOIP_CALL },
   { REGEX_EMAIL,     FLAVOR_EMAIL },
   { REGEX_NEWS_MAN,  FLAVOR_AS_IS },
+  { REGEX_LP,        FLAVOR_LP },
 };
 
 static GRegex **url_regexes;
