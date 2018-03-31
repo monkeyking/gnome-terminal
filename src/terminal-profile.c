8fc383ad8ae6e357a3e9af57c526f34154be2f3b
Ubuntu Git Importer <usd-importer-do-not-mail@canonical.com>
2018-03-31 04:12:49 +0000

    New upstream version 2.12.0

--- src/terminal-profile.c
+++ src/terminal-profile.c
@@ -1079,6 +1079,15 @@ terminal_profile_set_is_default (TerminalProfile *profile,
                            CONF_GLOBAL_PREFIX"/default_profile",
                            terminal_profile_get_name (profile),
                            NULL);
+
+  /* Even though the gconf change notification does this, it happens too late.
+   * In some cases, the default profile changes twice in quick succession,
+   * and update_default_profile must be called in sync with those changes.
+   */
+  update_default_profile (terminal_profile_get_name (profile),
+                          !gconf_client_key_is_writable (profile->priv->conf,
+                                                         CONF_GLOBAL_PREFIX"/default_profile",
+                                                         NULL));
 }
 
 void
@@ -3228,3 +3237,9 @@ terminal_palette_rxvt[TERMINAL_PALETTE_SIZE] =
   { 0, 0xffff, 0xffff, 0xffff }
 };
 
+void
+profile_name_entry_notify (TerminalProfile *profile)
+{
+  gconf_client_notify (profile->priv->conf,
+		       CONF_GLOBAL_PREFIX"/profile_list");
+}
